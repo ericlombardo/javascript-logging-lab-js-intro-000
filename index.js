19b22234ae61c0f4c032d6631c91926d5f4b921d
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-08-10 17:58:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+// Using console.error() to display an error message
 console.error("please try again")
+
+// Using console.log() to display a message to the user
+console.log("Pin entered correctly")
+
+// Using console.warn() to warn the user something may be wrong
